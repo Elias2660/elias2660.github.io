2aaf32f6936e5d465e68f1a102909f21d9fa30c4
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-01-23 11:29:50 -0500

    add additionall stuff for about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,9 +13,42 @@ export default function About() {
         </div>
         <div className="h-[100vh] w-full grid grid-cols-12 grid-rows-12" >
             <div className="relative inline-block col-start-2 col-span-full row-start-2 row-span-1 ">
-                <h1 className="text-5xl">
+                <h1 className="text-5xl underline">
                     Work Experience
                 </h1>
+                <span className="gap-x-0 mt-8 absolute opacity-75 inset-0 bottom-1 -z-10 bg-green-700 h-5 w-[25rem]" />
+            </div>
+            <div className="col-start-2 col-end-11 row-start-3 row-span-3">
+                <div className="h-full w-full grid grid-cols-12 grid-rows-2 gap-5">
+                    <div className="bg-slate-500 col-start-1 col-span-2 row-start-1 row-span-1" />
+                    <div className="col-start-3 col-span-full row-start-1 row-span-full">
+                        <h1 className="text-3xl">Researcher</h1>
+                        <p className="text-base">Rutgers Winlab</p>
+                        <p className="text-base">New Brunswick</p>
+                        <p className="text-sm">May 2021 - August 2021</p>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div className="h-[100vh] w-full grid grid-cols-12 grid-rows-12" >
+            <div className="relative inline-block col-start-2 col-span-full row-start-2 row-span-1 ">
+                <h1 className="text-5xl underline">
+                    Education
+                </h1>
+                <span className="gap-x-0 mt-8 absolute opacity-75 inset-0 bottom-1 -z-10 bg-orange-700 h-5 w-[15rem]" />
+            </div>
+            <div className="col-start-2 col-end-11 row-start-3 row-span-3">
+                <div className="h-full w-full grid grid-cols-12 grid-rows-2 gap-5">
+                    <div className="bg-slate-500 col-start-1 col-span-2 row-start-1 row-span-1" />
+                    <div className="col-start-3 col-span-full row-start-1 row-span-full">
+                        <h1 className="text-3xl">Researcher</h1>
+                        <p className="text-base">Rutgers Winlab</p>
+                        <p className="text-base">New Brunswick</p>
+                        <p className="text-sm">May 2021 - August 2021</p>
+                    </div>
+                </div>
             </div>
         </div>
     </>)
