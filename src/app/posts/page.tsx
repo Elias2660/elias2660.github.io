2f24a5782d85cb1d7a55073631549f69e0c8981d
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-01-22 23:35:28 -0500

    add comment

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,6 +2,7 @@
 export default function Posts() {
     return (<>
         <div>
+            {/* https://github.com/markdown-it/markdown-it */}
             <h1 className="text-4xl">Posts</h1>
             <p className="text-2xl">This is the Posts page</p>
         </div>
