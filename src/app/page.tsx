8ab2a5ce272601276e97c9a8aabf3889178345fd
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-01-22 22:17:04 -0500

    get inverting and mobile sizing for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ import PersonPhoto from "public/ProfilePic.png"
 export default function Home() {
     return (
         <div className="w-full h-[90vh] grid grid-cols-12 grid-rows-12 ">
-            <h1 className="z-10 col-start-2 col-span-4 row-start-3 text-6xl">Hi, I&apos;m Elias!</h1>
-            <p className="z-10 col-start-2 col-end-7 row-start-5 row-end-12 text-xl font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h1 className="z-10 col-start-2 col-span-full row-start-2 row-span-2 sm:col-start-2 sm:col-span-8 sm:row-start-3 text-6xl">Hi, I&apos;m Elias!</h1>
+            <p className="z-10 col-start-2 sm:text-lg row-start-6 col-end-10 sm:col-start-2    sm:col-end-7 sm:row-start-5 sm:row-end-12 md:text-xl font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             {/* ! IDK if imma add a photo or not */}
-            <Image src={PersonPhoto} alt="Elias Xu" className="z-0 col-start-7 col-span-5 row-start-2 row-span-8 w-fit h-fit" />
+            <Image src={PersonPhoto} alt="Elias Xu" className="col-start-4 col-span-8 row-start-3 row-span-full dark:brightness-50 z-0 sm:col-start-6 sm:col-span-5 sm:row-start-2 sm:row-span-8 w-fit h-fit" />
         </div>
     );
 }
