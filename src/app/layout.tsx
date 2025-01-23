12766f19b0175e52f9e760329c777cd69b4543c3
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-01-22 20:30:16 -0500

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,17 @@ import Footer from "@/components/footer";
 import "./globals.css";
 import Nav from "@/components/nav";
 import { Inter } from 'next/font/google';
+import { Metadata } from 'next';
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
     subsets: ['latin'],
     display: 'swap',
 });
+
+export const metadata: Metadata = {
+    title: "Elias Xu",
+    description: "Elias' Personal Website! Welcome :3",
+};
 
 
 export default function RootLayout({
@@ -20,6 +26,12 @@ export default function RootLayout({
     return (
         <html id="top" lang="en" className={themeClass}>
             <head>
+                <link
+                    rel="icon"
+                    href="/icon?<generated>"
+                    type="image/<generated>"
+                    sizes="<generated>"
+                />
             </head>
             <body
                 className={`dark:bg-black dark:text-white light:bg-white light:text-black  ${inter.className}`}
