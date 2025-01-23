788276766675a5de9b56fa6de442c91bf92e5c27
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-01-22 20:11:24 -0500

    Remove exclamation

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -4,7 +4,7 @@ export default function Portfolio() {
     return (<>
     <div>
         <h1 className="text-4xl">Portfolio</h1>
-        <p className="text-2xl">This is the Portfolio page!</p>
+        <p className="text-2xl">This is the Portfolio page</p>
     </div>
     </>)
 }
