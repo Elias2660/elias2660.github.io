f436770bed0674818955dc5288d21f3ceccd7458
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-01-23 22:54:29 -0500

    update

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,9 +1,13 @@
 
 // https://nextjs.org/learn-pages-router/basics/dynamic-routes/render-markdown
 
-// path: src/app/posts/[id]/page.tsx
+// path: 
 
-export default async function Page({ params }: { params: { id: Promise<string> } }) {
-    const id = await params.id;
-    return <div>My Post: {id}</div>;
+interface Params {
+    id: string;
+}
+
+export default async function page({ params }: { params: Promise<Params> }) {
+    const id = (await params).id;
+    return <div>My Post: {id}</div>
 }
