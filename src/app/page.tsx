8e7e63dcb73f02de30941dfe1641bfd609085cd3
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-01-27 21:32:50 -0500

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import PersonPhoto from "public/ProfilePic.png"
 
 export default function Home() {
     return (
-        <div className="w-full h-[40rem] grid grid-cols-12 grid-rows-12 ">
+        <div className="w-full h-[50rem] grid grid-cols-12 grid-rows-12 ">
             <div className="relative inline-block z-10 ml-4 col-start-1 col-span-full row-start-2 sm:row-start-1 row-span-2 md:col-start-1 md:col-span-full md:row-start-3 md:row-span-1 md:ml-4 lg:col-start-1 lg:col-end-10 lg:row-start-3">
                 <h1 className="text-5xl sm:text-6xl z-30">Hi, I&apos;m Elias! </h1>
                 <span className="gap-x-0 mt-6 sm:mt-10 absolute opacity-75 inset-0 bottom-1 -z-10 bg-yellow-300 h-5 w-[17rem] sm:w-[22rem]" />
             </div>
             <p className="grid z-10 col-start-1 col-span-full ml-4 mt-4 text-sm  sm:text-base row-start-9 row-span-full sm:col-start-2 sm:col-end-11 sm:mt-2 sm:row-start-8 md:col-start-1  mr-0  md:col-end-7 md:mr-2 md:row-start-4 md:row-end-10 md:text-xl md:ml-4 lg:row-start-4 lg:row-end-11 lg:col-start-1 lg:col-end-6 lg:mr-4 lg:ml-4 font-light">
-                Hi people of the internet, welcome to my website! I’m Elias, and I’m a bookworm, tech guy, and beekeeper. I’m currently a second semester senior at Stuyvesant High School, (and if all goes well) a rising freshman at Cornell University. I’m currently a researcher at Rutgers WINLAB, studying how machine learning can detect the behavior of untrained organisms, a beekeeper at various organizations, and a coder for different organizations :). The photo is from 2022, where I worked as an assistant beekeeper at the Brooklyn Bee Corps, which is part of City Growers.
+                Hi people of the internet, welcome to my website! I’m Elias, and I’m a bookworm, tech guy, and beekeeper. I’m currently a second semester senior at Stuyvesant High School, (and if I don&apos;t get recinded) a rising freshman at Cornell University. I’m currently a researcher at Rutgers WINLAB, studying how machine learning can detect the behavior of untrained organisms, a beekeeper at various organizations, and a coder for different organizations :). The photo is from 2022, where I worked as an assistant beekeeper at the Brooklyn Bee Corps, which is part of City Growers.
             </p>
             {/* ! IDK if imma add a photo or not */}
             <div className="relative flex items-start justify-center col-start-1 col-span-full row-start-3 mt-4 row-span-6 sm:col-start-1 sm:col-span-full sm:row-start-2 sm:mr-0 md:col-start-7 md:row-start-2 md:col-span-full md:mr-4 md:row-span-full lg:col-start-6 lg:col-span-6 lg:row-start-1 lg:row-span-full">
